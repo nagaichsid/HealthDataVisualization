05db32c36f4c9383113227bed96c28e4cb50204c
Sid Nagaich <nagaich.s@northeastern.edu>
2021-08-07 20:25:17 -0700

    Unfinished UnitTesting

---
+++ Sprint 2/src/main/java/Test/WalkingActivityTest.java
@@ -0,0 +1,77 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WalkingActivityTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void getId() {
+  }
+
+  @Test
+  public void setId() {
+  }
+
+  @Test
+  public void getPlaceLogId() {
+  }
+
+  @Test
+  public void setPlaceLogId() {
+  }
+
+  @Test
+  public void getDuration() {
+  }
+
+  @Test
+  public void setDuration() {
+  }
+
+  @Test
+  public void getDistance() {
+  }
+
+  @Test
+  public void setDistance() {
+  }
+
+  @Test
+  public void getStep() {
+  }
+
+  @Test
+  public void setStep() {
+  }
+
+  @Test
+  public void getCalorie() {
+  }
+
+  @Test
+  public void setCalorie() {
+  }
+
+  @Test
+  public void addDuration() {
+  }
+
+  @Test
+  public void addDistance() {
+  }
+
+  @Test
+  public void addStep() {
+  }
+
+  @Test
+  public void addCalorie() {
+  }
+}
